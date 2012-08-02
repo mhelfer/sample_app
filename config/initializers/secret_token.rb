f5500e03308c026780d4078a9a3949484a8a606d
Helfer, Michael <Michael.Helfer@bestbuy.com>
2012-08-01 23:48:03 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '288ea66d02075e6f412ac84ff62bb269d71d9c36a63a7c26a41c43330f6513bcd4bc081a482cc47d9bf64da039724cc1a3efb557b9a76f700672ec09abc1e905'
