c46448bf1425ed362b3ffdad93048943ee2fbf55
Helfer, Michael <Michael.Helfer@bestbuy.com>
2012-08-07 22:34:46 -0500

    Make a basic User model (including secure password)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.7'
 gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -12,6 +13,7 @@ gem 'bootstrap-sass', '2.0.0'
 group :development do
   gem 'sqlite3'
   gem 'rspec-rails'
+  gem 'annotate', '2.5.0'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -46,18 +46,18 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
   
-  # it "should have the right links on the layout" do
-  #   visit root_path
-  #   click_link "About"
-  #   page.should have_selector 'title', text: full_title('About')
-  #   click_link "Help"
-  #   page.should have_selector 'title', text: full_title('Help')
-  #   click_link "Contact"
-  #   page.should have_selector 'title', text: full_title('Contact')
-  #   click_link "Home"
-  #   click_link "Sign up now!"
-  #   page.should have_selector 'title', text: full_title('Sign up')
-  #   click_link "sample app"
-  #   page.should have_selector 'title', text: full_title('')
-  # end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
 end
